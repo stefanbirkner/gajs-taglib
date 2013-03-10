27892cb86ed1bcb72a2b1d24cf472a4aeb4f3186
Stefan Birkner <mail@stefan-birkner.de>
2013-03-10 18:43:01 +0100

    Remove multi-modul structure.

--- gajs4java/src/main/java/com/github/stefanbirkner/gajs4java/core/model/Command.java
+++
@@ -1,9 +0,0 @@
-package com.github.stefanbirkner.gajs4java.core.model;
-
-import java.util.List;
-
-public interface Command {
-	String getMethodName();
-
-	List<Object> getArguments();
-}
--- gajs4java/src/main/java/com/github/stefanbirkner/gajs4java/core/model/Protocol.java
+++
@@ -1,5 +0,0 @@
-package com.github.stefanbirkner.gajs4java.core.model;
-
-public enum Protocol {
-	HTTP, HTTPS, DECIDE_BY_RENDERER, DECIDE_BY_JAVASCRIPT
-}
